e75aedc5c311021fcb96cd1176b6841ed025e764
tonymmm1 <tony@mail.magnatox.com>
2020-10-15 17:17:01 -0500

    add solution

--- main.c
+++ main.c
@@ -2,5 +2,11 @@
 
 int main()
 {
+	int i = 10, *i2 = &i;
+	printf("value of address i: %d\n", *(&i));
+	printf("value of pointer i2: %d\n", *i2);
+	printf("address of i: %p\n", &i);
+	printf("address of i2: %p\n", &i2);
+	printf("address of i through pointer i2: %p\n", *(&i2));
 	return 0;
 }
