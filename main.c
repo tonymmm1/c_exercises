41b25f6e238afe11e6fe37b763f717bda9aac355
tonymmm1 <tony@mail.magnatox.com>
2020-10-15 17:32:07 -0500

    added solution

--- main.c
+++ main.c
@@ -2,5 +2,20 @@
 
 int main()
 {
+	int a, b, *ptra, *ptrb;
+	printf("Input a: ");
+	scanf("%d", &a);
+	printf("Input b: ");
+	scanf("%d", &b);
+
+	ptra = &a;
+	ptrb = &b;
+
+	if (*ptra > *ptrb)
+		printf("%d is the maximum number\n", *ptra);
+	else if (*ptrb > *ptra)
+		printf("%d is the maximum number\n", *ptrb);
+
+
 	return 0;
 }
