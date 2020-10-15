d347f5a7e9c02502edd7a77ad7ea3127a8d82d5a
tonymmm1 <tony@mail.magnatox.com>
2020-10-15 17:25:13 -0500

    added solution

--- main.c
+++ main.c
@@ -2,5 +2,19 @@
 
 int main()
 {
+	int a, b, *ptra, *ptrb, sum;
+	printf("Input a: ");
+	scanf("%d", &a);
+	printf("Input b: ");
+	scanf("%d", &b);
+
+	ptra = &a;
+	ptrb = &b;
+
+	sum = *ptra + *ptrb;
+
+	printf("Output sum: %d\n", sum);
+
+
 	return 0;
 }
