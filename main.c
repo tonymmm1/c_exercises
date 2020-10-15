992fdc4f831d84bd25007641abb359bbd7973229
tonymmm1 <tony@mail.magnatox.com>
2020-10-15 17:38:51 -0500

    added solution

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+/* array example using pointers */
+
 int main()
 {
+	int array[10], i;
+
+	/* initialize loop with variables */
+	for (i = 0; i < 10; i ++)
+	{
+		*(array + i) = i;
+	}
+
+	/* display elements of loop */
+	for (i = 0; i < 10; i++)
+		printf("element - %d : %d\n", i, *(array + i));
+
 	return 0;
 }
